e75d18cab2ffbdf8e9e4048e6c899166941d1f6c
ellina-s <ellina.sergeyeva@gmail.com>
2014-11-21 18:56:03 -0800

    Implemented DELETE for LEADSINGER

--- src/tables/LeadSinger.java
+++ src/tables/LeadSinger.java
@@ -2,6 +2,7 @@ package tables;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
 import com.mysql.jdbc.Connection;
 
 public class LeadSinger{
@@ -15,6 +16,10 @@ public class LeadSinger{
 		this.con = con;
 	}
 	
+	/*
+	 * Inserts a tuple in LEADSINGER.
+	 * Requires: an existing upc of a tuple from the ITEM table.
+	 */
 	public boolean insertLeadSinger( int upc, String name){
 		
 		PreparedStatement  ps;
@@ -36,6 +41,40 @@ public class LeadSinger{
 				return false;
 			} catch(SQLException e2) {
 				System.out.println("LeadSinger Rollback Error: " + e2.getMessage());
+				System.exit(-1);
+				return false;
+			}
+		}
+	}
+	
+	/*
+	 * Deletes a tuple from LeadSinger based on the item upc.
+	 */
+	public boolean deleteLeadSinger(int upc){
+		
+		try {
+			PreparedStatement ps = con.prepareStatement("DELETE FROM LeadSinger WHERE upc = ?");
+			ps.setInt(1, upc);
+			int rowCount = ps.executeUpdate();
+			if(rowCount == 0) {
+				System.out.println("LeadSinger for item # " + upc + " does not exist.");
+				ps.close();
+				return false;
+			}
+			con.commit();
+			System.out.println("LeadSinger for item # " + upc + " deleted successfully.");
+			ps.close();
+			return true;
+
+		} catch (SQLException e) {
+			System.out.println("LeadSinger Deletion Error: " + e.getMessage());
+			try {
+				con.rollback();
+				return false;
+			}
+			catch (SQLException ex2)
+			{
+				System.out.println("LeadSinger Deletion Rollback Error:: " + ex2.getMessage());
 				System.exit(-1);
 				return false;
 			}
--- tests/tablestests/leadSingerTest.java
+++ tests/tablestests/leadSingerTest.java
@@ -15,7 +15,7 @@ public class leadSingerTest{
 	private Connection con;
 	// ams represents a connection to an MySQL database
 	private DatabaseConnection ams = DatabaseConnection.getInstance();
-	private final static int ITEM_UPC = 100;
+	private final static int ITEM_UPC = 105;
 
 	/*
 	 * Tests insertion of a tuple in the LEADSINGER table.
@@ -34,7 +34,33 @@ public class leadSingerTest{
 		LeadSinger leadSinger = new LeadSinger(con);
 
 		// when
-		boolean status = leadSinger.insertLeadSinger(ITEM_UPC, "Elvis");
+		boolean status = leadSinger.insertLeadSinger(ITEM_UPC, "Sinatra");
+		// then
+		if(status == false){
+			fail();
+		}
+		assertEquals(true, status);
+	}
+	
+
+	/*
+	 * Tests deleting a tuple from LeadSinger.
+	 * Requires: upc should be an upc of an existing item.
+	 */
+	@Test
+	public void deleteLeadSinger(){
+		// given
+		if(ams.connect("root", "cs304pwd")){
+			System.out.println("You entered valid credentials.");
+		}
+		else{
+			System.out.println("Unable to connect.");
+		}
+		con = (Connection) ams.getConnection();
+		LeadSinger leadSinger = new LeadSinger(con);
+		
+		// when
+		boolean status = leadSinger.deleteLeadSinger(105);
 		// then
 		if(status == false){
 			fail();
