03ee2955b45e492584ada1678ce3dbf77de87f85
ellina-s <ellina.sergeyeva@gmail.com>
2014-11-21 18:47:02 -0800

    Implemented INSERT for the LEADSINGER table

--- src/tables/LeadSinger.java
+++ src/tables/LeadSinger.java
@@ -1,5 +1,45 @@
 package tables;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import com.mysql.jdbc.Connection;
+
 public class LeadSinger{
+	private Connection con;
+	
+	/*
+	 * Constructor for LeadSinger Table.
+	 * Takes a database connection as a parameter.
+	 */
+	public LeadSinger(Connection con){
+		this.con = con;
+	}
+	
+	public boolean insertLeadSinger( int upc, String name){
+		
+		PreparedStatement  ps;
+		try {
+			ps = con.prepareStatement("INSERT INTO leadsinger VALUES (?,?)");
+			ps.setInt(1, upc);
+			ps.setString(2, name);
+			
+			ps.executeUpdate();
+			con.commit();
+			System.out.println("LeadSinger ( " + upc + ", " + name + ") inserted successfully");
+			ps.close();
+			return true;
+
+		} catch (SQLException e1) {			
+			try {
+				con.rollback();
+				System.out.println("LeadSinger Insertion Error: " + e1.getMessage());
+				return false;
+			} catch(SQLException e2) {
+				System.out.println("LeadSinger Rollback Error: " + e2.getMessage());
+				System.exit(-1);
+				return false;
+			}
+		}
+	}
 	
 }
---
+++ tests/tablestests/leadSingerTest.java
@@ -0,0 +1,45 @@
+package tablestests;
+
+import static org.junit.Assert.*;
+import com.mysql.jdbc.Connection;
+import org.junit.Test;
+import tables.LeadSinger;
+import connection.DatabaseConnection;
+
+
+/**
+ * Tests for the LEADSINGER table basic operations.
+ */
+public class leadSingerTest{
+
+	private Connection con;
+	// ams represents a connection to an MySQL database
+	private DatabaseConnection ams = DatabaseConnection.getInstance();
+	private final static int ITEM_UPC = 100;
+
+	/*
+	 * Tests insertion of a tuple in the LEADSINGER table.
+	 * Requires: ITEM_UPC be an existing upc in the ITEM table.
+	 */
+	@Test
+	public void insertLeadSingerTest(){
+		// given
+		if(ams.connect("root", "cs304pwd")){
+			System.out.println("You entered valid credentials.");
+		}
+		else{
+			System.out.println("Unable to connect.");
+		}
+		con = (Connection) ams.getConnection();
+		LeadSinger leadSinger = new LeadSinger(con);
+
+		// when
+		boolean status = leadSinger.insertLeadSinger(ITEM_UPC, "Elvis");
+		// then
+		if(status == false){
+			fail();
+		}
+		assertEquals(true, status);
+	}
+
+}
