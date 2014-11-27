0d12d6c4e68900a253b7ae376aa05ee89037a6ba
ellina-s <ellina.sergeyeva@gmail.com>
2014-11-26 19:53:42 -0800

    Fixed some bugs with singer names

--- src/transactions/CustomerTransactions.java
+++ src/transactions/CustomerTransactions.java
@@ -278,12 +278,12 @@ public class CustomerTransactions{
 			else{
 
 				System.out.println("Found item(s) by singer");
-				System.out.println("----------------------- Generic Singer Search------------------------------------");
+				//System.out.println("----------------------- Generic Singer Search------------------------------------");
 
-				for(int i = 0; i < singers_upcs.size(); i++){
-					System.out.println(singers_upcs.get(i));
-				}
-				System.out.println(" ");
+				//for(int i = 0; i < singers_upcs.size(); i++){
+				//System.out.println(singers_upcs.get(i));
+				//}
+				//System.out.println(" ");
 
 				// Copy into upcs
 				for(int i = 0; i < singers_upcs.size(); i++){
@@ -300,13 +300,13 @@ public class CustomerTransactions{
 
 		ArrayList<ArrayList<String>> result = new ArrayList<ArrayList<String>> ();
 		result = populateReturnTable(upcs, table, name);
-		
-		System.out.println("----------------------- Resulting upcs list------------------------------------");
-		for(int k = 0; k< upcs.size(); k++){
-			System.out.println(upcs.get(k));
-		}
-		System.out.println(" ");
-		
+
+		//System.out.println("----------------------- Resulting upcs list------------------------------------");
+		//for(int k = 0; k< upcs.size(); k++){
+		//System.out.println(upcs.get(k));
+		//}
+		//System.out.println(" ");
+
 		return dataTransform(result);
 	}
 
@@ -327,7 +327,7 @@ public class CustomerTransactions{
 		Statement stmt;
 		ResultSet rs;
 		boolean singerRecorded = false;
-		
+
 		String statement = "SELECT * FROM Item Where upc = ";
 
 		try
@@ -387,36 +387,40 @@ public class CustomerTransactions{
 
 				if(rs.next()) {
 					name = rs.getString("name");
-
-					if(singername.equals(name)){
+					if(singername.equals("")){
+						//System.out.println("Detected emtpy singer string");
 						table.get(7).add(name);
-						System.out.println("Recorded singer " + name  +" for " +  upcs.get(i));
-						//System.out.println("singer name: " + name + " upc " + upcs.get(i));
+						//System.out.println("Recorded singer " + name  +" for " +  upcs.get(i));
 					}
 					else{
+						if(singername.equals(name) || singername.equals(name.toLowerCase())){
+							table.get(7).add(name);
+							//System.out.println("Recorded singer " + name  +" for " +  upcs.get(i));
+							//System.out.println("singer name: " + name + " upc " + upcs.get(i));
+						}
+						else{
 
-						while(rs.next()){
-							String anothername = rs.getString("name");
+							while(rs.next()){
+								String anothername = rs.getString("name");
+								if(singername.equals(anothername) || singername.equals(anothername.toLowerCase())){
+									table.get(7).add(anothername);
+									//System.out.println("Recorded singer " + anothername  +"  for " +  upcs.get(i));
+									singerRecorded = true;
+									//System.out.println("singer name: " + anothername + " upc " + upcs.get(i));
+								}
+							}
 
-							if(singername.equals(anothername)){
-								table.get(7).add(anothername);
-								System.out.println("Recorded singer " + anothername  +"  for " +  upcs.get(i));
-								singerRecorded = true;
-								//System.out.println("singer name: " + anothername + " upc " + upcs.get(i));
+							if(!singerRecorded){
+								table.get(7).add("N/A"); // if no more singers found, then assign 'N/A"
+								//System.out.println("Recorded singer NA for " +  upcs.get(i) + " (after while loop)");
 							}
 						}
-						
-						if(!singerRecorded){
-							table.get(7).add("N/A"); // if no more singers found, then assign 'N/A"
-							System.out.println("Recorded singer NA for " +  upcs.get(i) + " (after while loop)");
-						}
-						
-						
 					}
+
 				}
 				else{
 					table.get(7).add("N/A");
-					System.out.println("Recorded singer NA for " +  upcs.get(i));
+					//System.out.println("Recorded singer NA for " +  upcs.get(i));
 					//System.out.println(" * No singer found for upc: " +  upcs.get(i));
 				}
 			}
@@ -436,25 +440,16 @@ public class CustomerTransactions{
 	 * Transforms a 2D array list into 2D string array.
 	 */
 	private String[][] dataTransform(ArrayList<ArrayList<String>> table) {
-
-		System.out.println(" *** Table size: " + Integer.toString(table.size()));
-		System.out.println(" *** Table 0th elem size: " + Integer.toString(table.get(0).size()));
-
 		String[][] result = new String[table.get(0).size()][table.size()];
 		for(int i = 0; i <table.size(); i++){
-			
-			System.out.println("sub-size:" + table.get(i).size());
 			for(int j = 0; j <table.get(i).size(); j++){
-				
-				System.out.println("i: " + i + " j: " + j);
-				
 				result[j][i] = table.get(i).get(j);
 			}
 		}
 		return result;
 	}
-	
-	
+
+
 
 	// TODO
 
